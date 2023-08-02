150cbe9cc54bed19abaf71a2b18bbae380706463
lutao <lutaoapply2023@gmail.com>
2023-08-01 20:53:35 -0400

    react_axios

---
+++ src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <main>
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        This blog app is a porject in the learn React tutorial series
+      </p>
+    </main>
+  );
+};
+
+export default About;
